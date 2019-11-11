50dc8226b893345f63e234bdf9d217bddd7dac46
Marcin Zadka <gamboox@gmail.com>
2019-11-11 22:47:27 +0100

    Add IP Stack lib

---
+++ lib/ip_stack/client.rb
@@ -0,0 +1,49 @@
+require 'net/http'
+require 'resolv'
+
+module IPStack
+  class Client
+    API_URL = 'http://api.ipstack.com/'.freeze
+    API_KEY = ENV['IPSTACK_API_KEY']
+    IP_REGEX  = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/
+
+    def initialize
+    end
+
+    def by_name(name)
+      name =~ IP_REGEX ? by_ip(name) : by_url(name)
+    end
+
+    def by_ip(ip)
+      response = request(ip)
+      {
+        ip: response['ip'],
+        latitude: response['latitude'],
+        longitude: response['longitude']
+      }
+    end
+
+    def by_url(url)
+      ip = Resolv.getaddress(url)
+      response = request(ip)
+      {
+        ip: response['ip'],
+        name: url,
+        latitude: response['latitude'],
+        longitude: response['longitude']
+      }
+    end
+
+    private
+
+    def request(path)
+      url = API_URL + path + "?access_key=#{API_KEY}"
+      uri = URI(url)
+      req = Net::HTTP::Get.new(uri)
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: false) do |http|
+        http.request(req)
+      end
+      ActiveSupport::JSON.decode(res.body)
+    end
+  end
+end
